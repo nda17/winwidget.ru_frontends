a9043ac949aa468536d8a0279d734ac35c099d63
Dmitry <khv1702@gmail.com>
2026-09-08 14:00:22 +0300

    style(payment): align desktop card height across products

--- apps/widgets/src/screens/payment/ui/pricing/CrmPricingCards.tsx
+++ apps/widgets/src/screens/payment/ui/pricing/CrmPricingCards.tsx
@@ -16,6 +16,7 @@ import styles from './CrmPricingCards.module.scss'
 const rubles = new Intl.NumberFormat('ru-RU', {
 	style: 'currency',
 	currency: 'RUB',
+	minimumFractionDigits: 0,
 	maximumFractionDigits: 2
 })
 
